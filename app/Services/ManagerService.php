6ae59a65e2e17e41aaaa367ebc0096c16983e6dc
delimce <delimce@gmail.com>
2020-06-15 22:44:01 +0200

    UUID generation for users & manager's tokens

--- app/Services/ManagerService.php
+++ app/Services/ManagerService.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class ManagerService
 {
-    const TOKEN_LENGHT = 40;
 
     /**
      * checkEmail
@@ -256,24 +256,6 @@ class ManagerService
      */
     public static function newUserToken()
     {
-        return sprintf(
-            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
-            // 16 bits for "time_hi_and_version",
-            // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
-            // 16 bits, 8 bits for "clk_seq_hi_res",
-            // 8 bits for "clk_seq_low",
-            // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
-            // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
-        );
+        return Str::orderedUuid();
     }
 }
